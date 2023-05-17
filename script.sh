4656733eaa57c6cc9bb717325712fbc6801c426c
Alhonainy21 <92266474+Alhonainy21@users.noreply.github.com>
2023-05-17 09:33:24 -0500

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt-get update -y
+apt-get install nano -y
+apt install net-tools -y
+apt install python3-pip -y
+pip install torch==1.11.0
+pip install torchvision==0.12.0
+pip install flwr==0.16.0
+pip install numpy==1.22.1
+pip install tqdm
