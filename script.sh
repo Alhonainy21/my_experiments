f9ccde920afc4ba382ad36c11581944c1997fa81
Alhonainy21 <92266474+Alhonainy21@users.noreply.github.com>
2023-05-26 20:19:23 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,9 +2,11 @@
 apt-get update -y
 apt-get install nano -y
 apt install net-tools -y
+apt-get install curl -y
 apt install python3-pip -y
 pip install torch==1.11.0
 pip install torchvision==0.12.0
 pip install flwr==0.16.0
 pip install numpy==1.22.1
 pip install tqdm
+
