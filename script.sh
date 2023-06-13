db0a4622cfc55d2a332f0378ffdba4693ef9cba8
Alhonainy21 <92266474+Alhonainy21@users.noreply.github.com>
2023-06-12 22:14:02 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,9 +9,3 @@ pip install torchvision==0.12.0
 pip install flwr==0.16.0
 pip install numpy==1.22.1
 pip install tqdm
-cd /
-curl -L -o sss https://www.dropbox.com/s/e189wlk7kkdlhgs/lung_80.tar.gz?dl=0
-tar -zxvf sss
-mv lung80,20 lung80
-cd lung80
-find -name '._*' -delete
